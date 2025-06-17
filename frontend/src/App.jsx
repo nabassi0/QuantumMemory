4fbaa87c4191e049b417bbd6706bb6770894a30d
ABASSI Naier (SNCF VOYAGEURS / TECHNICENTRE SUD EST EUROPEEN / MD2I - Informatique) <0020695V@commun.ad.sncf.fr>
2025-06-17 15:57:23 +0200

    making things again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header";
+import Header from "./components/Header.jsx";
 import Container from "@mui/material/Container";
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Header />
       <Container maxWidth="sm">
-          <h1> This is content</h1>    
+          <h1 className="text"> This is content</h1>    
       </Container>
     </div>
   );
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,14 @@
 import "../styles/components/Header.scss";
-import Button from "@mui/material/Button";
+import Drawer from "../components/Drawer";
+import Logo from "../assets/images/LogoQuantum.svg"
 
 export default function Header() {
   return (
     <div className="header">
       <div className="header-container">
-        <h1>This is the header.</h1>
-      <Button variant="contained">Nouvelle partie</Button>
+        <img src={Logo} alt="" />
+          <Drawer />
       </div>
-      
     </div>
   );
 }
