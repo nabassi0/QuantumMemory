65163a974a9e1f856dd169570b960a8f367d3fcb
naier abassi <naierabassi@gmail.com>
2025-06-18 11:23:47 +0200

    modifying css again

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,13 @@
 import "../styles/components/Header.scss";
 import Drawer from "../components/Drawer";
-import Logo from "../assets/images/LogoQuantum.svg"
+import Logo from "../assets/images/oui.svg";
 
 export default function Header() {
   return (
     <div className="header">
       <div className="header-container">
-        <img src={Logo} alt="" />
-          <Drawer />
+        <img src={Logo} alt="" className="background-image" />
+        <Drawer />
       </div>
     </div>
   );
