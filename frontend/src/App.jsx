e2013c4b2550f7e530caa81e34305461322b2d38
naier abassi <naierabassi@gmail.com>
2025-06-18 10:43:42 +0200

    fixing css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header.jsx";
+import Header from "./components/Header";
 import Container from "@mui/material/Container";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Container maxWidth="sm">
-          <h1 className="text"> This is content</h1>    
-      </Container>
+      <main className="main-content">
+        <Container maxWidth="sm">
+          <div className="content">
+            <h1 className="text"> This is content</h1>
+          </div>
+        </Container>
+      </main>
     </div>
   );
 }
